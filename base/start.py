94fa86fd9e1f0a118c2ed953dd162a4f63269678
Alex Bachurin <bachurin.alex@gmail.com>
2022-12-20 13:34:07 +0000

    *

--- base/start.py
+++ base/start.py
@@ -171,21 +171,15 @@ working_directory = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(working_directory, 'config.json'), 'r') as f:
     CONF = json.load(f)
 LOCAL = os.environ.get('S3_URL', 'local')
-if LOCAL == 'local':
-    with open(CONF['s3_credentials'], 'r') as f:
-        s3_credentials = json.load(f)
-    with open(CONF['mongo_credentials'], 'r') as f:
-        mongo_credentials = json.load(f)
-else:
-    s3_credentials = {
-        'region': os.environ.get('S3_REGION', None),
-        'access_key': os.environ.get('S3_ACCESS_KEY', None),
-        'secret_key': os.environ.get('S3_SECRET_KEY', None)
-    }
-    mongo_credentials = {
-        'user': os.environ.get('MG_USER', None),
-        'pwd': os.environ.get('MG_PWD', None)
-    }
+s3_credentials = {
+    'region': os.environ.get('S3_REGION', None),
+    'access_key': os.environ.get('S3_ACCESS_KEY', None),
+    'secret_key': os.environ.get('S3_SECRET_KEY', None)
+}
+mongo_credentials = {
+    'user': os.environ.get('MG_USER', None),
+    'pwd': os.environ.get('MG_PWD', None)
+}
 
 S3 = Storage(s3_credentials)
 DB = Mongo(mongo_credentials)
